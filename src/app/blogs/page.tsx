d6e781ed2801ebcb286d45d9980c1a09317c025e
unknown <smdemon3@gmail.com>
2024-04-26 23:48:36 +0600

    setup redux

--- src/app/blogs/page.tsx
+++ src/app/blogs/page.tsx
@@ -1,11 +1,17 @@
+"use client"
+
 import BlogCard from "@/components/ui/BlogCard";
+import { useGetBlogsQuery } from "@/redux/api/baseApi";
 import { Blog } from "@/types";
 
-const BlogsPage = async () => {
-  const res = await fetch("http://localhost:5000/blogs", {
-    cache: "no-store",
-  });
-  const blogs = await res.json();
+const BlogsPage = () => {
+  // const res = await fetch("http://localhost:5000/blogs", {
+  //   cache: "no-store",
+  // });
+  // const blogs = await res.json();
+  const { data: blogs, isLoading, isError, error } = useGetBlogsQuery("");
+  console.log(blogs);
+
   return (
     <div className="w-[90%] mx-auto my-6">
       <h1 className="text-4xl text-center my-3">
@@ -18,7 +24,7 @@ const BlogsPage = async () => {
         </i>
       </p>
       <div className="grid grid-cols-3 gap-6 mt-10">
-        {blogs.map((blog: Blog) => (
+        {blogs?.map((blog: Blog) => (
           <BlogCard key={blog?.id} blog={blog} />
         ))}
       </div>
